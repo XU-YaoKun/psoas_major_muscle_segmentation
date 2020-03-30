dd315b498f7c2332b878f00353b91cb04375ce87
XXXU <xu_yaokun@163.com>
2020-03-30 23:22:17 +0800

    modify dataset

--- modules/data/dataset.py
+++ modules/data/dataset.py
@@ -1,67 +1,43 @@
 import os
 import os.path as osp
+import pickle
 
 import torch
 from torch.utils.data import Dataset
 import numpy as np
-
-import nibabel
 
 
 class BaseDataset(Dataset):
     def __init__(self, data_path):
         super(BaseDataset, self).__init__()
 
-        self.image_path = osp.join(data_path, "imgs")
-        self.image_list = sorted(os.listdir(self.image_path))
-        self.label_path = osp.join(data_path, "labels")
-        self.label_list = sorted(os.listdir(self.label_path))
-
-        self.num_images = len(self.image_list)
-
-        # load sample image
-        sample_img = self._read(
-            osp.join(self.image_path, self.image_list[-1])
+        self.image_data = self._load(
+            osp.join(data_path, 'imgs.pickle')
         )
-        self.img_shape = sample_img.shape
-        self.z = self.img_shape[-1]
-
-        print("Total images: {}".format(self.num_images))
-        print("Image shape: {}".format(self.img_shape))
-        print(
-            "Total training samples: [{} x {} = {}]".format(
-                self.num_images,
-                self.z,
-                self.num_images * self.z,
-            )
+        self.label_data = self._load(
+            osp.join(data_path, 'labels.pickle')
         )
+
+        self.image_size = self.image_data.shape[:2]
+        self.len = self.image_data.shape[2]
+
+        print("Image shape: {}".format(self.image_size))
+        print("Total images: {}".format(self.len))
 
     def __len__(self):
-        return self.num_images * self.z
+        return self.len
 
     @staticmethod
-    def _read(file):
-        instance = nibabel.load(file)
-        data = instance.get_fdata()
+    def _load(file):
+        with open(file, 'rb') as f:
+            data = pickle.load(f)
         return data
 
     def __getitem__(self, item):
-        image_idx = int(item / self.z)
-        z_idx = int(item % self.z)
+        image = self.image_data[:, :, item].astype(np.float32)
+        label = self.label_data[:, :, item].astype(np.float32)
 
-        _image = self._read(
-            osp.join(
-                self.image_path, self.image_list[image_idx]
-            )
-        )
-        _label = self._read(
-            osp.join(
-                self.label_path, self.label_list[image_idx]
-            )
-        )
-
-        image = _image[:, :, z_idx].astype(np.float32)
-        label = _label[:, :, z_idx].astype(np.float32)
+        # transform
 
         data_batch = {"image": image, "label": label}
         return data_batch
---
+++ preprocess.py
@@ -0,0 +1,37 @@
+import os
+import os.path as osp
+import pickle
+
+import nibabel
+import numpy as np
+
+
+def _read(file):
+    instance = nibabel.load(file)
+    data = instance.get_fdata()
+    return data
+
+
+def _dump(data_path, name):
+    file_list = sorted(os.listdir(data_path))
+
+    data = np.empty([512, 512, 0], dtype=np.float32)
+    for file in file_list:
+        imgs = _read(osp.join(data_path, file))
+        data = np.concatenate([data, imgs], axis=2)
+
+    dump_path = osp.join('data', name)
+    with open(dump_path, 'wb') as f:
+        pickle.dump(data, f)
+
+    print("Dump {} to {}".format(data_path, dump_path))
+    return
+
+
+def main():
+    _dump(osp.join('data', 'imgs'), 'imgs.pickle')
+    _dump(osp.join('data', 'labels'), 'labels.pickle')
+
+
+if __name__ == "__main__":
+    main()
