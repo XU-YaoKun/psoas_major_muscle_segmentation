7ca65bcc10cc89f5b158a3eefe80dcceb06f9012
XXXU <xu_yaokun@163.com>
2020-04-24 20:54:39 +0800

    refactorize fcn

--- modules/model/fcn/fcn.py
+++ modules/model/fcn/fcn.py
@@ -1,7 +1,21 @@
 import torch
 import torch.nn as nn
 
-import numpy as np
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(ConvBlock, self).__init__()
+
+        self.conv = nn.Conv2d(in_channel, out_channel, 3, padding=1)
+        self.bn = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+
+        return x
 
 
 class FCN(nn.Module):
@@ -9,70 +23,38 @@ class FCN(nn.Module):
         super(FCN, self).__init__()
 
         # conv1
-        self.conv1_1 = nn.Conv2d(n_channels, 64, 3, padding=1)
-        self.bn1_1 = nn.BatchNorm2d(64)
-        self.relu1_1 = nn.ReLU(inplace=True)
-        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
-        self.bn1_2 = nn.BatchNorm2d(64)
-        self.relu1_2 = nn.ReLU(inplace=True)
+        self.conv1_1 = ConvBlock(n_channels, 64)
+        self.conv1_2 = ConvBlock(64, 64)
         self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
         # conv2
-        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
-        self.bn2_1 = nn.BatchNorm2d(128)
-        self.relu2_1 = nn.ReLU(inplace=True)
-        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
-        self.bn2_2 = nn.BatchNorm2d(128)
-        self.relu2_2 = nn.ReLU(inplace=True)
+        self.conv2_1 = ConvBlock(64, 128)
+        self.conv2_2 = ConvBlock(128, 128)
         self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
         # conv3
-        self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
-        self.bn3_1 = nn.BatchNorm2d(256)
-        self.relu3_1 = nn.ReLU(inplace=True)
-        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
-        self.bn3_2 = nn.BatchNorm2d(256)
-        self.relu3_2 = nn.ReLU(inplace=True)
-        self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
-        self.bn3_3 = nn.BatchNorm2d(256)
-        self.relu3_3 = nn.ReLU(inplace=True)
+        self.conv3_1 = ConvBlock(128, 256)
+        self.conv3_2 = ConvBlock(256, 256)
+        self.conv3_3 = ConvBlock(256, 256)
         self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
         # conv4
-        self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
-        self.bn4_1 = nn.BatchNorm2d(512)
-        self.relu4_1 = nn.ReLU(inplace=True)
-        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
-        self.bn4_2 = nn.BatchNorm2d(512)
-        self.relu4_2 = nn.ReLU(inplace=True)
-        self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
-        self.bn4_3 = nn.BatchNorm2d(512)
-        self.relu4_3 = nn.ReLU(inplace=True)
+        self.conv4_1 = ConvBlock(256, 512)
+        self.conv4_2 = ConvBlock(512, 512)
+        self.conv4_3 = ConvBlock(512, 512)
         self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
  
-        # conv5
-        self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
-        self.bn5_1 = nn.BatchNorm2d(512)
-        self.relu5_1 = nn.ReLU(inplace=True)
-        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
-        self.bn5_2 = nn.BatchNorm2d(512)
-        self.relu5_2 = nn.ReLU(inplace=True)
-        self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
-        self.bn5_3 = nn.BatchNorm2d(512)
-        self.relu5_3 = nn.ReLU(inplace=True)
-        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
-
         # fcn6
-        self.fc6 = nn.Conv2d(512, 4096, 1)
+        self.fc6 = nn.Conv2d(512, 1024, 1)
         self.relu6 = nn.ReLU(inplace=True)
         self.drop6 = nn.Dropout2d()
 
         # fcn7
-        self.fc7 = nn.Conv2d(4096, 4096, 1)
+        self.fc7 = nn.Conv2d(1024, 1024, 1)
         self.relu7 = nn.ReLU(inplace=True)
         self.drop7 = nn.Dropout2d()
 
-        self.score_fr = nn.Conv2d(4096, n_class, 1)
+        self.score_fr = nn.Conv2d(1024, n_class, 1)
         self.score_pool3 = nn.Conv2d(256, n_class, 1)
         self.score_pool4 = nn.Conv2d(512, n_class, 1)
 
@@ -97,30 +79,25 @@ class FCN(nn.Module):
 
     def forward(self, x):
         h = x
-        h = self.bn1_1(self.relu1_1(self.conv1_1(h)))
-        h = self.bn1_2(self.relu1_2(self.conv1_2(h)))
+        h = self.conv1_1(h)
+        h = self.conv2_1(h)
         h = self.pool1(h)
 
-        h = self.bn2_1(self.relu2_1(self.conv2_1(h)))
-        h = self.bn2_2(self.relu2_2(self.conv2_2(h)))
+        h = self.conv2_1(h)
+        h = self.conv2_2(h)
         h = self.pool2(h)
 
-        h = self.bn3_1(self.relu3_1(self.conv3_1(h)))
-        h = self.bn3_2(self.relu3_2(self.conv3_2(h)))
-        h = self.bn3_3(self.relu3_3(self.conv3_3(h)))
+        h = self.conv3_1(h)
+        h = self.conv3_2(h)
+        h = self.conv3_3(h)
         h = self.pool3(h)
         pool3 = h  # 1/8
 
-        h = self.bn4_1(self.relu4_1(self.conv4_1(h)))
-        h = self.bn4_2(self.relu4_2(self.conv4_2(h)))
-        h = self.bn4_3(self.relu4_3(self.conv4_3(h)))
+        h = self.con4_1(h)
+        h = self.con4_2(h)
+        h = self.con4_3(h)
         h = self.pool4(h)
         pool4 = h  # 1/16
-
-        h = self.bn5_1(self.relu5_1(self.conv5_1(h)))
-        h = self.bn5_2(self.relu5_2(self.conv5_2(h)))
-        h = self.bn5_3(self.relu5_3(self.conv5_3(h)))
-        h = self.pool5(h)
 
         h = self.relu6(self.fc6(h))
         h = self.drop6(h)
